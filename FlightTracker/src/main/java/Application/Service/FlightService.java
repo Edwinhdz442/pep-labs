39efb2de9c246bd21ad892daffb778ac78cfb43e
Edwin Hernandez <edwinhdz442@gmail.com>
2023-01-23 21:09:56 +0000

    Saved progress on FlightTracker 2023-01-23 21:09:56 +0000

--- FlightTracker/src/main/java/Application/Service/FlightService.java
+++ FlightTracker/src/main/java/Application/Service/FlightService.java
@@ -61,7 +61,13 @@ public class FlightService {
      *         user should have some insight if they attempted to edit a nonexistent flight.)
      */
     public Flight updateFlight(int flight_id, Flight flight){
-        return null;
+        Flight flightFromDb = this.flightDAO.getFlightById(flight_id);
+
+        if(flightFromDb == null) return null;
+        
+        flightDAO.updateFlight(flight_id,flight);
+        return this.flightDAO.getFlightById(flight_id);
+        //return null;
     }
 
     /**
