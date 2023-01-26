36a893ff59026cbf07af687f9c070d5642b6b08f
Edwin Hernandez <edwinhdz442@gmail.com>
2023-01-26 06:39:17 +0000

    Saved progress on CombineArrays 2023-01-26 06:39:17 +0000

--- ContainsDuplicates/src/main/java/ContainsDuplicates.java
+++ ContainsDuplicates/src/main/java/ContainsDuplicates.java
@@ -1,4 +1,4 @@
-
+import java.util.HashSet;
 
 public class ContainsDuplicates {
     /**
@@ -8,6 +8,7 @@ public class ContainsDuplicates {
      * @return true if nums contains any duplicate values, false if it does not.
      */
     public boolean containsDuplicate(int[] nums){
+        
         return false;
     }
 }
