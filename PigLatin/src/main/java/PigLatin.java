084fcb95d8d8aa73cb42a2de12c34ed80be3ff4f
Edwin Hernandez <edwinhdz442@gmail.com>
2023-01-31 23:11:47 +0000

    Saved progress on PigLatin 2023-01-31 23:11:47 +0000

--- PigLatin/src/main/java/PigLatin.java
+++ PigLatin/src/main/java/PigLatin.java
@@ -17,6 +17,9 @@ public class PigLatin {
      * @return the pig latin form of in.
      */
     public String returnPigLatin(String in){
-        return "";
+       
+        String a = in.substring(1, in.length());
+        String pig = a + in.charAt(0) + "ay";
+        return pig;
     }
 }
